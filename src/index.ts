5bc099351a49ed49a05966359e58552e92ae264b
Saurav Kumar <ksauravhash@gmail.com>
2025-01-22 16:55:45 +0530

    cors setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import 'dotenv/config'
 import { connectDB } from "./db";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(cors());
 
 const port = process.env.PORT;
 

44c9dee8eefcc8b9d5fa6b530f83d00b23b5ff8b
Saurav Kumar <ksauravhash@gmail.com>
2025-01-23 14:05:10 +0530

    small fix

--- src/utility/auth.ts
+++ src/utility/auth.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const accessSecret = process.env.ACCESS_TOKEN_SECRET;
-const refreshSecret = process.env.REFRESH_TOKEN_SECRET;
+const accessSecret = process.env.ACCESS_TOKEN_SECRET!;
+const refreshSecret = process.env.REFRESH_TOKEN_SECRET!;
 
 export const generateRefreshToken = (payload: string | Buffer | object) => {
   return jwt.sign(payload, refreshSecret, { expiresIn: "7d" });

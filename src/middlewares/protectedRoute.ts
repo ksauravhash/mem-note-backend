cc488b32646300e908486dc835b0e4d948498259
Saurav Kumar <ksauravhash@gmail.com>
2025-01-25 13:48:39 +0530

    procted route middleware

---
+++ src/middlewares/protectedRoute.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { verifyAccessToken } from "../utility/auth";
+import { JwtPayload } from "jsonwebtoken";
+
+const protectedRoute = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    const authToken = authHeader && authHeader.split(" ")[1];
+    if (!authToken) {
+      res.sendStatus(401);
+      return;
+    }
+    req.user = verifyAccessToken(authToken) as { id: string; username: string; name: string };
+    next();
+  } catch (err) {
+    res.status(500).send();
+    console.error(
+      `${new Date().toTimeString()} ${new Date().toDateString()}`,
+      err
+    );
+  }
+};

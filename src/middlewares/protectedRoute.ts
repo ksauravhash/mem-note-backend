74591a837f984179afdc9d48c11a51fbf5085c95
Saurav Kumar <ksauravhash@gmail.com>
2025-01-26 16:10:16 +0530

    update error handling for protected route

--- src/middlewares/protectedRoute.ts
+++ src/middlewares/protectedRoute.ts
@@ -1,7 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { verifyAccessToken } from "../utility/auth";
+import jwt from "jsonwebtoken";
 
-export const protectedRoute = (req: Request, res: Response, next: NextFunction) => {
+export const protectedRoute = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const authHeader = req.headers["authorization"];
     const authToken = authHeader && authHeader.split(" ")[1];
@@ -9,10 +14,20 @@ export const protectedRoute = (req: Request, res: Response, next: NextFunction) 
       res.sendStatus(401);
       return;
     }
-    req.user = verifyAccessToken(authToken) as { id: string; username: string; name: string };
+    req.user = verifyAccessToken(authToken) as {
+      id: string;
+      username: string;
+      name: string;
+    };
     next();
   } catch (err) {
-    res.status(500).send();
+    if (
+      err instanceof jwt.JsonWebTokenError ||
+      err instanceof jwt.TokenExpiredError ||
+      err instanceof jwt.NotBeforeError
+    )
+      res.sendStatus(401);
+    else res.sendStatus(500);
     console.error(
       `${new Date().toTimeString()} ${new Date().toDateString()}`,
       err
